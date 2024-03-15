a2ecd924555d67479b051df46cf2eaa6cd77c8d8
bernaaaaak <bernaaaak@gmail.com>
2024-03-15 22:17:49 +0300

    dev

--- app.js
+++ app.js
@@ -2,6 +2,17 @@
 
 const express = require("express");
 const app = express();
+/* ------------------------------------------------------- */
+
+const cors = require('cors');
+app.use(cors({
+    origin: 'http://localhost : 3000'
+}));
+app.get('/products/:id', function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for all origins!'})
+  })
+
+/* ------------------------------------------------------- */
 
 require("dotenv").config();
 require("express-async-errors")
@@ -14,8 +25,8 @@ app.use(express.json())
 // Catch async-errors:
 require('express-async-errors')
 /* ------------------------------------------------------- */,
-// Routes:
-app.use(require('./app/routes/todo.router'))
+    // Routes:
+    app.use(require('./app/routes/todo.router'))
 /* ------------------------------------------------------- */
 // ErrorHandler:
 app.use(require('./app/errorHandler'))
