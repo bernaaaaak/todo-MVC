fb33be305bd8c38e293695a4c70fe8b330e77b60
AdamSmith <omercoskun4369@gmail.com>
2024-03-15 22:05:12 +0300

    branch created

--- app.js
+++ app.js
@@ -3,23 +3,24 @@
 const express = require("express");
 const app = express();
 
+console.log("Merhaba Dünya");
+
 require("dotenv").config();
-require("express-async-errors")
+require("express-async-errors");
 
 const PORT = process.env.PORT || 8000;
 
 // Accept json data and convert object:
-app.use(express.json())
+app.use(express.json());
 
 // Catch async-errors:
-require('express-async-errors')
-/* ------------------------------------------------------- */,
-// Routes:
-app.use(require('./app/routes/todo.router'))
+require("express-async-errors"),
+  /* ------------------------------------------------------- */ // Routes:
+  app.use(require("./app/routes/todo.router"));
 /* ------------------------------------------------------- */
 // ErrorHandler:
-app.use(require('./app/errorHandler'))
+app.use(require("./app/errorHandler"));
 
 app.listen(PORT, () => {
-    console.log("App listening on port " + PORT);
+  console.log("App listening on port " + PORT);
 });
