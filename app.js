f72aceecfdc737b1cdf4251170ef8e2919ad6e4c
mucahitkarakus <mocococo.80@gmail.com>
2024-03-15 22:04:19 +0300

    Refactor app.js file

--- app.js
+++ app.js
@@ -8,16 +8,12 @@ require("express-async-errors")
 
 const PORT = process.env.PORT || 8000;
 
-// Accept json data and convert object:
 app.use(express.json())
 
-// Catch async-errors:
 require('express-async-errors')
-/* ------------------------------------------------------- */,
-// Routes:
+
 app.use(require('./app/routes/todo.router'))
-/* ------------------------------------------------------- */
-// ErrorHandler:
+
 app.use(require('./app/errorHandler'))
 
 app.listen(PORT, () => {
